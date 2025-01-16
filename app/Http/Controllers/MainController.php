8f136b1d98095917759b96fb05f0daf8c72f2deb
Daniel Damasceno Meira <daniel.meira@campus.unimontes.int>
2025-01-16 13:39:42 -0300

    making some test with query builder

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,13 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
     public function index(){
-        DB::connection('mysql')->getPdo();
-        echo "OK";
+        
+        // tests
+        $products = DB::table('products')
+                        ->whereIn('id', [1,3,5])
+                        ->get();
+
+        // $this->showRawData($results);
+        $this->showDataTable($products);
+    }
+
+    private function showRawData($data)
+    {
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+    }
+
+    private function showDataTable($data)
+    {
+        echo '<table border="1">';
+
+        echo '<tr>';
+
+        foreach ($data[0] as $key => $value) {
+            echo '<th>' . $key . '</th>';
+        }
+
+        echo '</tr>';
+
+        foreach ($data as $row) {
+            echo '<tr>';
+            foreach ($row as $key => $value) {
+                echo '<td>' . $value . '</td>';
+            }
+            echo '</tr>';
+        }
+
+        echo '</table>';
     }
 }
